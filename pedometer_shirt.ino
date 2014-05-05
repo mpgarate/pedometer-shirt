f18fbc7758fc11dc37baea365eb347619b9f25fa
Michael Garate <mpgarate@gmail.com>
2014-05-05 13:39:07 -0400

    starting to write a pedometer

---
+++ pedometer_shirt.ino
@@ -0,0 +1,31 @@
+const int pedometerPin = A5;
+
+void setup(){
+  Serial.begin(9600);
+}
+int pedoVal;
+int lastPedoVal = 0;
+int steps = 0;
+int foundStep = false;
+int foundTime = 0;
+int elapsedTime = 0;
+
+void loop(){
+  pedoVal = analogRead(pedometerPin);
+    
+  if (pedoVal != lastPedoVal){
+    elapsedTime = millis() - foundTime;
+    if (elapsedTime > 100){
+      steps++;
+    }
+    
+    foundTime = millis();
+    
+    //Serial.println(elapsedTime);
+    Serial.println(steps);
+  }
+  
+  
+  
+  lastPedoVal = pedoVal;
+}
